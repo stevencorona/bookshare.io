3ccb1524c240f051e62e045610a277114d94de09
Steve Corona <stevecorona@me.com>
2014-03-14 13:09:30 -0400

    initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookshare::Application.config.secret_key_base = '56e84bec013b3c6583ca5cda168fbb7217c0878984ec532622c8cca09914ce78881dc4601c3b5d0d1aea55ce7f8ca53281f62aaaa8a5a585ff51452c12911b33'
