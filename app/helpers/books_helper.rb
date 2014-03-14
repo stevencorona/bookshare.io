bc4126807a207da908f8afbb32a43f22a5727e84
Steve Corona <stevecorona@me.com>
2014-03-14 15:22:36 -0400

    add book covers from amazon s3

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -2,4 +2,10 @@ module BooksHelper
   def amazon_link(book, opts={})
     link_to "View on Amazon", "https://www.amazon.com/gp/product/#{book.isbn}", opts
   end
+
+  def book_cover_image(book, opts={})
+    opts[:width]  ||= 200
+    image_tag "https://s3.amazonaws.com/thumbs.bookshare.io/#{book.isbn}/cover.jpg", opts
+  end
+
 end
