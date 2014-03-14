11eae2f5aa66bf3aec244f24e58c5171e505cf36
Steve Corona <stevecorona@me.com>
2014-03-14 14:41:25 -0400

    add empty books controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Bookshare::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'books#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -12,7 +12,7 @@ Bookshare::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :books
 
   # Example resource route with options:
   #   resources :products do
