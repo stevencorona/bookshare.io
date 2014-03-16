740e849bb42c7036f0aa938af2b4a9dce8266e45
Steve Corona <stevecorona@me.com>
2014-03-16 15:23:54 -0400

    add working blog link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def nav_link(text, path)
+  def nav_link(text, path, opts={})
     active_class = current_page?(path) ? :active : ''
 
     content_tag(:li, class: active_class) do
-      link_to text, path
+      link_to text, path, opts
     end
-    
+
   end
 end
