b84d2dd170708c08c1051e10bdf725a076d84eb2
Steve Corona <stevecorona@me.com>
2014-03-16 22:58:09 -0400

    add category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140317015537) do
+ActiveRecord::Schema.define(version: 20140317025735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 20140317015537) do
   end
 
   add_index "books", ["isbn"], name: "index_books_on_isbn", using: :btree
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "items", force: true do |t|
     t.integer  "order_id"
