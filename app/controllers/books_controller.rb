d2f90cbb63d39e705e62612b267eeb3272a61f88
Steve Corona <stevecorona@me.com>
2014-03-17 22:43:06 -0400

    order books by created at until we get ranking

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   protect_from_forgery except: [:create]
 
   def index
-    @books = Book.where(status: :avaliable).page(params[:page]).per_page(BOOKS_PER_PAGE)
+    @books = Book.where(status: :avaliable).order("created_at desc").page(params[:page]).per_page(BOOKS_PER_PAGE)
   end
 
   def category
