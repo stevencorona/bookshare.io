45fa372f1e5f7a734f3c14e17f4c2b9d811c4a3b
Steve Corona <stevecorona@me.com>
2014-04-06 15:08:59 -0400

    make printable order pages

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,5 @@
 class Admin::OrdersController < AdminController
   def index
-    @orders = Order.where("status = ?", :paid)
+    @orders = Order.where("status = ?", :paid).order("updated_at")
   end
 end
